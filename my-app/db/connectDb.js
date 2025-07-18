3291ffa33356918ce8824955f156d6aeb401f24d
singhaniayuvraj11 <singhaniayuvraj11@gmail.com>
2025-07-18 21:13:57 +0530

    add

--- my-app/db/connectDb.js
+++ my-app/db/connectDb.js
@@ -1,18 +1,17 @@
-
+import 'dotenv/config';
 import mongoose from "mongoose";
 
 const connectDb = async () => {
-        try {
-            const conn = await mongoose.connect("mongodb+srv://singhaniayuvraj11:Yuvraj123@getmeabeer.qviu9hs.mongodb.net/beer?retryWrites=true&w=majority&appName=getmeabeer", {
-                useNewUrlParser: true,
-            });
-            console.log(`MongoDB Connected: ${conn.connection.host}`);
-            return conn;
-            
-        } catch (error) {
-            console.error(error.message);
-            process.exit(1);
-        }
+    try {
+        const conn = await mongoose.connect(process.env.MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log(`MongoDB Connected: ${conn.connection.host}`);
+    } catch (error) {
+        console.error("MongoDB connection error:", error);
+        process.exit(1);
     }
+}
 
-  export default connectDb;
+export default connectDb;
