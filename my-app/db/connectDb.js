d1440104a3947b06d4cbe2af2865072be9eddda5
singhaniayuvraj11 <singhaniayuvraj11@gmail.com>
2025-07-19 14:58:16 +0530

    Update connectDb.js

--- my-app/db/connectDb.js
+++ my-app/db/connectDb.js
@@ -1,17 +1,18 @@
-import 'dotenv/config';
+
 import mongoose from "mongoose";
 
 const connectDb = async () => {
-    try {
-        const conn = await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
-        console.log(`MongoDB Connected: ${conn.connection.host}`);
-    } catch (error) {
-        console.error("MongoDB connection error:", error);
-        process.exit(1);
+        try {
+            const conn = await mongoose.connect(process.env.MONGO_URI, {
+                useNewUrlParser: true,
+            });
+            console.log(`MongoDB Connected: ${conn.connection.host}`);
+            return conn;
+            
+        } catch (error) {
+            console.error(error.message);
+            process.exit(1);
+        }
     }
-}
 
-export default connectDb;
+  export default connectDb;
