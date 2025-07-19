7bb913aa8c0b0071feb4c33dfb26081e792f7677
singhaniayuvraj11 <singhaniayuvraj11@gmail.com>
2025-07-19 15:20:00 +0530

    j

--- my-app/components/PaymentPage.js
+++ my-app/components/PaymentPage.js
@@ -31,7 +31,7 @@ const PaymentPage = ({ username }) => {
   } else {
     getData();
   }
-}, [session, router, getData]); // Include dependencies
+}, [session, router,]); // Include dependencies
 
 
   useEffect(() => {
